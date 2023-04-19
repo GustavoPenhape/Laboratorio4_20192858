4bab7408a25647577aaf7d0fdac399937aa10925
Gustavo <a20192858@pucp.edu.pe>
2023-04-18 22:01:17 -0500

    SUBIDA

--- laboratorio4_20192858/src/main/java/com/example/laboratorio4_20192858/Controller/HomeController.java
+++ laboratorio4_20192858/src/main/java/com/example/laboratorio4_20192858/Controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.example.laboratorio4_20192858.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Login")
 public class HomeController {
+    @GetMapping("")
+    public String home(){
+        return "login";
+    }
 }
